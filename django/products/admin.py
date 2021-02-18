aeb352de7439dab5c3e04ea9f007d63f2e2e155a
Zojan01 <yojan_alejandro@hotmail.com>
2021-02-18 12:42:09 -0400

    register models to admin & create super user -> admin admin

--- django/products/admin.py
+++ django/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import House,TypeProdcut,Novel
 # Register your models here.
+admin.site.register(House)
+admin.site.register(TypeProdcut)
+admin.site.register(Novel)
