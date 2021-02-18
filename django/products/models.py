2d59643471b41cef994a5c908bd26885c1192b17
Zojan01 <yojan_alejandro@hotmail.com>
2021-02-18 12:18:25 -0400

    add models and migrations

--- django/products/models.py
+++ django/products/models.py
@@ -4,23 +4,32 @@ from django.db import models
 
 
 class TypeProdcut(models.Model):
-    name = models.CharField();
+    name = models.CharField(max_length=150);
     fieldsType = models.TextField();
     fieldsName = models.TextField();
 
 class Novel(models.Model):
     price   = models.IntegerField();   
-    title   = models.CharField();
-    author = models.CharField();
+    name   = models.CharField(max_length=150);
+    author = models.CharField(max_length=150);
     summary = models.TextField(null=False, blank=False);
     ageLimit = models.IntegerField();
     pathPoster = models.TextField();
+    TypeProdcut = models.ForeignKey(TypeProdcut, on_delete=models.CASCADE, null=True);
+    date_created = models.DateTimeField(auto_now_add=True)
+
 
 class House(models.Model):
    price   = models.IntegerField();
-   name    = models.CharField();
-   condition = models.CharField();
+   name    = models.CharField(max_length=150);
+   condition = models.CharField(max_length=150);
+   location = models.TextField();
    sumary = models.TextField(null=False, blank=False);
    production_year = models.DateField();
+   pathPoster = models.TextField();
+   amountRoon = models.IntegerField();
+   typeProdcut = models.ForeignKey(TypeProdcut, on_delete=models.CASCADE, null=True);
+   date_created = models.DateTimeField(auto_now_add=True)
+
 
    
