65f798d7f67c66d2672ea7b75ee0bfd64c37937f
Zojan01 <yojan_alejandro@hotmail.com>
2021-02-18 09:44:00 -0400

    add work place for angular

--- django/products/models.py
+++ django/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    price = models.IntegerField(); 
+    pathPoster = models.CharField();
+
+    title = models.CharField(max_length=150)
+    summary = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
+class Novel(models.Model):
+    price = models.models.IntegerField();   
+    title
+   
+
